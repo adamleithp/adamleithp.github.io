ca4d9caec380c96d06ae155c38d0a80e8cd6c060
adam <adamleithp@me.com>
2014-07-14 13:53:49 -0400

    new css styles

---
+++ _site/travel2/footer.php
@@ -0,0 +1,15 @@
+
+</div>
+<div class="overlay overlay--loading"></div>
+
+</body>
+
+<script src='app/scripts/jquery.js'></script>
+<script src='app/scripts/scroller.min.js'></script>
+<script src='app/scripts/foundation.min.js'></script>
+<script src='app/scripts/app.min.js'></script>
+
+<script type="text/javascript">
+//Place analytics code here
+</script>
+</html>
